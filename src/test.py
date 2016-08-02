f99a554b436356669efb9588d08dcf40baf5d425
bijanfallah <bijan.fallah@gmail.com>
2016-08-02 15:43:55 +0200

    test loop added

--- src/test.py
+++ src/test.py
@@ -4,9 +4,10 @@ Plot_CCLM(bcolor='black')
 
 Plot_CCLM(dir_mistral='/work/bb0962/work1/work/member/post/', name='lffd1979010100c.nc',
           bcolor='red', var='HSURF', flag='FALSE', color_map='FALSE')
+for i in xrange(1,8):
+    Plot_CCLM(dir_mistral='/work/bb0962/work1/work/member0'+i+'/post/', name='lffd1979010100c.nc',
+          bcolor='gray', var='HSURF', flag='FALSE', color_map='FALSE')
 
-Plot_CCLM(dir_mistral='/work/bb0962/work1/work/member02/post/', name='lffd1979010100c.nc',
-          bcolor='red', var='HSURF', flag='FALSE', color_map='FALSE')
 
 pdf='test'
 plt.savefig("Figure_" +pdf+ ".pdf")
